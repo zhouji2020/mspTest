b99682e66b29b525d7887c412bd2bf0274615bad
zhouji2020 <76575143+zhouji2020@users.noreply.github.com>
2021-02-25 18:33:13 +0800

    完善注释

--- common/Log.py
+++ common/Log.py
@@ -14,28 +14,28 @@ class Logger(object):
         self.log_file_name = 'logs'  # 日志文件的名称
         self.backup_count = 5  # 最多存放日志的数量
         self.console_output_level = 'WARNING'  # 控制台输出级别
-        self.file_output_level = 'DEBUG'  # 日志输出级别
+        self.file_output_level = 'DEBUG'  # 日志文件输出级别
         self.formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')  # 日志输出格式
 
     def get_logger(self):
         """在logger中添加日志句柄并返回，如果logger已有句柄，则直接返回"""
         if not self.logger.handlers:  # 避免重复日志
-            console_handler = logging.StreamHandler()
-            console_handler.setFormatter(self.formatter)
-            console_handler.setLevel(self.console_output_level)
-            self.logger.addHandler(console_handler)
+            console_handler = logging.StreamHandler()  # 将日志流输出到console_handler对象
+            console_handler.setFormatter(self.formatter)  # 设置console_handler对象格式
+            console_handler.setLevel(self.console_output_level)  # 设置console_handler对象日志等级
+            self.logger.addHandler(console_handler)  # 将console_handler对象添加到日志处理中
 
             file_handler = TimedRotatingFileHandler(filename=os.path.join(log_path, self.log_file_name), when='D',
                                                     interval=1, backupCount=self.backup_count, delay=True,
                                                     encoding='utf-8')
-            # 每天重新创建一个日志文件，最多保留backup_count份
-            file_handler.setFormatter(self.formatter)
-            file_handler.setLevel(self.file_output_level)
-            self.logger.addHandler(file_handler)
-        return self.logger
+            # 日志文件处理方式：每天重新创建一个日志文件，最多保留backup_count份
+            file_handler.setFormatter(self.formatter)  # 日志文件格式
+            file_handler.setLevel(self.file_output_level)  # 日志文件输出日志等级
+            self.logger.addHandler(file_handler)  # 日志文件添加到日志处理中
+        return self.logger  # 返回获取到的日志
 
 
-logger = Logger().get_logger()
+logger = Logger().get_logger()  # 定义一个日志输出对象
 
 
 # import os
